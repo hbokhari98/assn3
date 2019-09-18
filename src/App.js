618112f66c486df3d269d20a9482b519f48a0d20
Hareem Bokhari <hbokhari98@gmail.com>
2019-09-17 23:47:48 -0400

    fetch and display

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   constructor() {
       super();
       this.state ={
-        pictures: [],
+        url:''
       };
   }
 componentDidMount() {
   fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   .then(results => {
       return results.json();
-  }).then(data => {
-    let pictures = data.results.map((pic) => {
-      return(
-        <div key={pic.results}>
-          <img src={pic.url} />
-          </div>
-    )
-  })
-})
-}
+  }).then(data => this.setState({url: data.url}));
+  }
 
 
   render() {
   return (
     <div className="App">
-      {this.state.pictures}
+      <img src={this.state.url} />
     </div>
   );
   }
