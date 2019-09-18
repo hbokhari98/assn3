224ae5332784c46a7ca56e87d2ffc4230ca26a35
Hareem Bokhari <hbokhari98@gmail.com>
2019-09-18 19:47:06 -0400

    add title

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ class App extends Component {
   constructor() {
       super();
       this.state ={
-        url:''
+        url:'',
+        title: ''
       };
   }
 componentDidMount() {
   fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   .then(results => {
       return results.json();
-  }).then(data => this.setState({url: data.url}));
+  }).then(data => this.setState({
+    url: data.url,
+    title: data.title
+  }));
   }
 
 
   render() {
   return (
     <div className="App">
+      <h1>{this.state.title}</h1>
       <img src={this.state.url} />
     </div>
   );
